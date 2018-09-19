bbdc5d3ce449aa88505f244f3f8881f29825d2db
Hayden Braxton <hayden.braxton.dev@gmail.com>
2018-09-19 12:43:18 -0400

    pollish bob-is-cool for better demo

--- src/examples/bob-is-cool.component.js
+++ src/examples/bob-is-cool.component.js
@@ -1,9 +1,24 @@
 const template = document.createElement('template');
 const styles = `
-p {
-	color: #f8981c;
+:host {
+	--color: #f8981c
 }
 
+p {
+	color: var(--color, '#6f7dbc');
+}
+
+slot::slotted(small) {
+	color: yellow;
+}
+
+slot[name="secondary-slot"]::slotted(small) {
+	color: red;
+}
+
+slot[name="secondary-slot"]::slotted(.cool) {
+	color: #6f7dbc;
+}
 `;
 template.innerHTML = `
 <style>${styles}</style>
@@ -13,6 +28,21 @@ template.innerHTML = `
 <slot name="secondary-slot"></slot>
 `;
 
+// template tag vs innerHTML
+// lifecycle events
+// attribute change
+// attrs vs. props
+// attr reflection?
+// design like native elements
+
+// w/o shadow
+// <slot>
+// :host selector
+// ::slotted selector
+// ::slotted nesting
+// low presedence
+// light DOM vs shadow DOM
+
 class BobIsCool extends HTMLElement {
 	constructor() {
 		super();
@@ -21,7 +51,7 @@ class BobIsCool extends HTMLElement {
 		this._name = '';
 
 		this._changeHanlders = {};
-		this._changeHanlders.name = (oldValue, newValue) => this.handleNameChange(oldValue, newValue);
+		this._changeHanlders['name'] = (oldValue, newValue) => this.handleNameChange(oldValue, newValue);
 	}
 
 	handleNameChange(oldValue, newValue) {
@@ -33,10 +63,13 @@ class BobIsCool extends HTMLElement {
 	}
 
 	connectedCallback() {
+		// this.appendChild(template.content.cloneNode(true));
+		// this._$name = this.querySelector('#name');
+
 		const shadowRoot = this.attachShadow({ mode: 'open' });
 		shadowRoot.appendChild(template.content.cloneNode(true));
-
 		this._$name = shadowRoot.querySelector('#name');
+
 		this.name = this.getAttribute('name') || 'Bob';
 	}
 
